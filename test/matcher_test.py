2e5cea4d17724eacb97ab8819f2b46c5e3554330
fmakdemir <1462379+fmakdemir@users.noreply.github.com>
2025-02-08 23:18:01 +0000

    feat: reset the git history

---
+++ test/matcher_test.py
@@ -0,0 +1,9 @@
+from fwordlesolver.matcher import match
+
+
+def test_matches():
+    assert match("brighter", "arabised") == "?x?...x."
+    assert match("alabired", "brighter") == "...???x."
+    assert match("alrbirer", "brighter") == "..???.xx"
+    assert match("alrbired", "brighter") == "..????x."
+    assert match("achy", "hast") == "?.?."
